68132b40d892fcc16ec45f21149931f3bd07d738
0226zy <grbh0226@gmail.com>
2026-05-28 17:26:50 +0800

    add

--- solutions/hot100/0300_longest_increasing_subsequence/longest_increasing_subsequence_test.go
+++ solutions/hot100/0300_longest_increasing_subsequence/longest_increasing_subsequence_test.go
@@ -43,6 +43,11 @@ func TestLengthOfLIS(t *testing.T) {
 			nums: []int{5, 4, 3, 2, 1},
 			want: 1,
 		},
+		{
+			name: "乱序",
+			nums: []int{1, 2, 4, 3},
+			want: 3,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
