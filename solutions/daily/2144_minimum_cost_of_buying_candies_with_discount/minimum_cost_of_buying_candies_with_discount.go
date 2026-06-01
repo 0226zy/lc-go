61cc1bcb202a866f022a3cbfaaeffeb82f7046ae
karlyzhang <karlyzhang@tencent.com>
2026-06-01 17:54:25 +0800

    daily: implement 2144 minimum cost of buying candies with discount

--- solutions/daily/2144_minimum_cost_of_buying_candies_with_discount/minimum_cost_of_buying_candies_with_discount.go
+++ solutions/daily/2144_minimum_cost_of_buying_candies_with_discount/minimum_cost_of_buying_candies_with_discount.go
@@ -1,9 +1,22 @@
 package mincostcandies
 
+import "sort"
+
 // MinimumCost 打折购买糖果的最小开销
 // 每购买两颗糖果，第三颗免费（免费糖果价格不超过两颗中较便宜的那颗）。
 // 返回购买所有糖果的最小开销。
 // 时间复杂度: O(n log n)  空间复杂度: O(1)
 func MinimumCost(cost []int) int {
-	return 0
+
+	sort.Slice(cost, func(i, j int) bool {
+		return cost[i] > cost[j]
+	})
+
+	ret := 0
+	for i, c := range cost {
+		if (i+1)%3 != 0 {
+			ret += c
+		}
+	}
+	return ret
 }
